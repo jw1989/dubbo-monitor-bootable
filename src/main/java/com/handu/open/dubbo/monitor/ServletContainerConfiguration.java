e797347d36fd543ef396e3c9309483acdd338939
Yin Weihong <yinwh@gjmetal.com>
2018-05-02 17:13:05 +0800

    initilized

---
+++ src/main/java/com/handu/open/dubbo/monitor/ServletContainerConfiguration.java
@@ -0,0 +1,18 @@
+package com.handu.open.dubbo.monitor;
+
+import org.springframework.boot.context.embedded.undertow.UndertowEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.undertow.UndertowOptions;
+
+@Configuration
+public class ServletContainerConfiguration {
+
+    @Bean
+    public UndertowEmbeddedServletContainerFactory embeddedServletContainerFactory() {
+        UndertowEmbeddedServletContainerFactory factory = new UndertowEmbeddedServletContainerFactory();
+        factory.addBuilderCustomizers(builder -> builder.setServerOption(UndertowOptions.ENABLE_HTTP2, true));
+        return factory;
+    }
+}
