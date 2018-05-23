6ae44de3b79da2c7d48adb394288d21757e7406b
Yin Weihong <yinwh@gjmetal.com>
2018-05-23 18:57:35 +0800

    升级spring2版本

--- src/main/java/com/handu/open/dubbo/monitor/ServletContainerConfiguration.java
+++ src/main/java/com/handu/open/dubbo/monitor/ServletContainerConfiguration.java
@@ -1,6 +1,7 @@
 package com.handu.open.dubbo.monitor;
 
-import org.springframework.boot.context.embedded.undertow.UndertowEmbeddedServletContainerFactory;
+import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,9 +11,9 @@ import io.undertow.UndertowOptions;
 public class ServletContainerConfiguration {
 
     @Bean
-    public UndertowEmbeddedServletContainerFactory embeddedServletContainerFactory() {
-        UndertowEmbeddedServletContainerFactory factory = new UndertowEmbeddedServletContainerFactory();
-        factory.addBuilderCustomizers(builder -> builder.setServerOption(UndertowOptions.ENABLE_HTTP2, true));
+    public ServletWebServerFactory embeddedServletContainerFactory() {
+        UndertowServletWebServerFactory factory = new UndertowServletWebServerFactory();
+        factory.addBuilderCustomizers(builder -> builder.setServerOption(UndertowOptions.ENABLE_HTTP2, true).setServerOption(UndertowOptions.HTTP2_SETTINGS_ENABLE_PUSH, true));
         return factory;
     }
 }
